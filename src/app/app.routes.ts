18bb5e9972cde4aca2bb064663a2f853e0d9a4a7
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-09-29 23:05:34 +0300

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: "home", component: HomePageComponent},
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {path: "profile", component: ProfilePageComponent},
+];
