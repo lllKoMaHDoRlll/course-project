18ce68b0aec56e2f17a038344a1433bd6e1436f9
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-10-01 16:36:12 +0300

    add ton connect button to profile card

--- src/app/profile-card/profile-card.component.ts
+++ src/app/profile-card/profile-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { UserData } from '../user-data';
+import { TonConnectButtonComponent } from '../ton-connect-button/ton-connect-button.component';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
-  imports: [],
+  imports: [TonConnectButtonComponent],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss'
 })
