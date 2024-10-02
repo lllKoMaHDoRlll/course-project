7f68950105f7517bd2b1e7e4b139d5378204a148
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-10-02 09:27:14 +0300

    add /exercises route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { ExercisesPageComponent } from './pages/exercises-page/exercises-page.component';
 
 
 export const routes: Routes = [
   {path: "home", component: HomePageComponent},
   {path: "", redirectTo: "home", pathMatch: "full"},
   {path: "profile", component: ProfilePageComponent},
+  {path: "exercises", component: ExercisesPageComponent}
 ];
