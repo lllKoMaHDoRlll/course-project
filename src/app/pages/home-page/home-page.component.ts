f1ae2dfdabcfcbc364a9cbe6fabda3929451f7f7
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-10-02 08:38:03 +0300

    implement home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ButtonComponent, ColorThemes } from '../../components/button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+  router = new Router();
+  buttonsThemes = [ColorThemes.PRIMARY, ColorThemes.ACCEPT, ColorThemes.NEUTRAL];
 
+  goTo = (path: string) => {
+    this.router.navigate([path]);
+  }
 }
