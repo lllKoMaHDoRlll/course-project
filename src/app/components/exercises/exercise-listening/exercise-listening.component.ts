2f2a0bf8117aba0d8e3c639a8e9b3178503b2f5e
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-10-06 10:54:01 +0300

    implement exercise listening component

---
+++ src/app/components/exercises/exercise-listening/exercise-listening.component.ts
@@ -0,0 +1,72 @@
+import { AfterViewInit, Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlay, faCircleCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { ButtonComponent } from '../../button/button.component';
+import { exercisesService } from '../../../services/database/exercises.service';
+import { ExerciseListeningData } from '../../../interfaces/exercises-data';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from '../../loading/loading.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-exercise-listening',
+  standalone: true,
+  imports: [FontAwesomeModule, ButtonComponent, CommonModule, LoadingComponent],
+  templateUrl: './exercise-listening.component.html',
+  styleUrl: './exercise-listening.component.scss'
+})
+export class ExerciseListeningComponent implements AfterViewInit {
+  exerciseListeningData: ExerciseListeningData | undefined;
+  audio: HTMLAudioElement | undefined;
+  answerInput: HTMLInputElement | undefined;
+  answerResult: -2 | -1 | 0 | 1 = -2;
+
+  faPlay = faPlay;
+  faCircleCheck = faCircleCheck;
+  faCircleXmark = faCircleXmark;
+
+  constructor(private router: Router) {}
+
+  async ngAfterViewInit() {
+    this.exerciseListeningData = await exercisesService.getRandomExerciseListeningData();
+
+    setTimeout(() => {
+      this.audio = document.querySelector(".exercise-card__audio > audio") as HTMLAudioElement;
+      this.answerInput = document.querySelector(".exercise-card__answer > input") as HTMLInputElement;
+    }, 500);
+  }
+
+  play = (ev: MouseEvent) => {
+    console.log(1);
+    this.audio!.play();
+  }
+
+  handleAnswer = async () => {
+    console.log(this.answerInput!.value);
+    const answer = this.answerInput!.value;
+    if (!answer) return;
+    this.answerResult = -1;
+
+    const res = await exercisesService.checkListeningAnswer(1, answer);
+    console.log(res);
+    if (res) {
+      this.answerResult = 1;
+    }
+    else this.answerResult = 0;
+  }
+
+  goBack = () => {
+    this.router.navigate(['exercises']);
+  }
+
+  resetExercise = async () => {
+    this.exerciseListeningData = undefined;
+    this.answerResult = -2;
+    this.exerciseListeningData = await exercisesService.getRandomExerciseListeningData();
+
+    setTimeout(() => {
+      this.audio = document.querySelector(".exercise-card__audio > audio") as HTMLAudioElement;
+      this.answerInput = document.querySelector(".exercise-card__answer > input") as HTMLInputElement;
+    }, 500);
+  }
+}
