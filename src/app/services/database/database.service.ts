87fe4692aff1e9ffe5060bea30a93a39adfaa062
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-10-27 16:27:08 +0300

    implement database service

--- src/app/services/database/database.service.ts
+++ src/app/services/database/database.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import axios from "axios";
+
+const DB_HOST = "https://tonolingo.ru";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,24 @@ import { Injectable } from '@angular/core';
 export class DatabaseService {
 
   constructor() { }
+
+  getUserProfilePicture = async (userId: number): Promise<string> => {
+    const result = await axios.get(`${DB_HOST}/api/telegram/profile_photo?user_id=${userId}`, {
+      responseType: "blob"
+    });
+    console.log(result);
+    const href = URL.createObjectURL(result.data);
+    return href;
+  }
+
+  createOrUpdateUser = async (userId: number, wallet: string | null = null) => {
+    const result = await axios.post(`${DB_HOST}/api/users`, {
+      user_id: userId,
+      wallet: wallet
+    });
+  }
+
+  updateVisitStatus = async (userId: number) => {
+    const result = await axios.post(`${DB_HOST}/api/achievements/visits?user_id=${userId}`);
+  }
 }
