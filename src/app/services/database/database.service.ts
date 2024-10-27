b0b65fd498ddf453bcefea900f3f784ca2b91f8e
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-10-27 19:00:27 +0300

    implement getAchievementsTypesProgresses and getAchievements database methods

--- src/app/services/database/database.service.ts
+++ src/app/services/database/database.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import axios from "axios";
+import { AchievementProgress, Achievement } from '../../interfaces/achievements';
 
 const DB_HOST = "https://tonolingo.ru";
 
@@ -28,5 +29,20 @@ export class DatabaseService {
 
   updateVisitStatus = async (userId: number) => {
     const result = await axios.post(`${DB_HOST}/api/achievements/visits?user_id=${userId}`);
+  }
+
+  getAchievementsTypesProgresses = async (userId: number): Promise<AchievementProgress[]> => {
+    const result = await axios.get(`${DB_HOST}/api/achievements/types?user_id=${userId}`);
+    return result.data.result as AchievementProgress[];
+  }
+
+  getAchievements = async (userId: number, achievementTypeId: number): Promise<Achievement[]> => {
+    const result = await axios.get(`${DB_HOST}/api/achievements`, {
+      params: {
+        "user_id": userId,
+        "type_id": achievementTypeId
+      }
+    });
+    return result.data.result as Achievement[];
   }
 }
