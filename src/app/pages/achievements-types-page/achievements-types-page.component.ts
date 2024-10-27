235d5aa779bf5be8e8a129413e4ad7b55cf7499f
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-10-27 18:59:42 +0300

    implement achievements-types-page component

---
+++ src/app/pages/achievements-types-page/achievements-types-page.component.ts
@@ -0,0 +1,46 @@
+import { AchievementProgressCardComponent } from '../../components/achievement-progress-card/achievement-progress-card.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { AchievementProgress } from '../../interfaces/achievements';
+import { faHeadphones, faLanguage, faLink, faParagraph, faSpellCheck, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
+import { TelegramService } from '../../services/telegram.service';
+import { DatabaseService } from '../../services/database/database.service';
+import { Router } from '@angular/router';
+
+const achievementsTypesIcons = [
+  faStar,
+  faParagraph,
+  faLanguage,
+  faHeadphones,
+  faSpellCheck,
+  faLink
+];
+
+@Component({
+  selector: 'app-achievements-types-page',
+  standalone: true,
+  imports: [ CommonModule, FontAwesomeModule, AchievementProgressCardComponent],
+  templateUrl: './achievements-types-page.component.html',
+  styleUrl: './achievements-types-page.component.scss'
+})
+export class AchievementsTypesPageComponent implements OnInit{
+  database = inject(DatabaseService);
+  telegram = inject(TelegramService);
+  achievementsProgressesList: AchievementProgress[] = [];
+
+  constructor(private router: Router) {
+    this.telegram.showBackButton();
+  }
+
+  async ngOnInit() {
+    this.achievementsProgressesList = (await this.database.getAchievementsTypesProgresses(this.telegram.getUserTGId()!)).sort((a, b) => a.id - b.id);
+    for (let i = 0; i < this.achievementsProgressesList.length; i++) {
+      this.achievementsProgressesList[i].icon = achievementsTypesIcons[i];
+    }
+  }
+
+  navigateTo(id: number) {
+    this.router.navigate(["achievements", id])
+  }
+}
