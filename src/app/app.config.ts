fcc64f15512fc7d89e62c7327cc1c4ff7c4f7342
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-11-01 11:24:18 +0300

    remove telegram service from app providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { TelegramService } from './services/telegram.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), TelegramService]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
