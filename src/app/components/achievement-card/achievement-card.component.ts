b287d515e21662a224cf0e50de533be73bce7a2b
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-11-09 10:26:39 +0300

    implement modal on claiming SBT

--- src/app/components/achievement-card/achievement-card.component.ts
+++ src/app/components/achievement-card/achievement-card.component.ts
@@ -3,18 +3,24 @@ import { Achievement } from '../../interfaces/achievements';
 import { ButtonComponent } from "../button/button.component";
 import { TelegramService } from '../../services/telegram.service';
 import { DatabaseService } from '../../services/database/database.service';
+import TonConnectService from '../../services/ton-connect.service';
+import { ModalComponent } from '../modal/modal.component';
+import { LoadingForComponent } from '../loading-for/loading-for.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-achievement-card',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ModalComponent, LoadingForComponent, CommonModule],
   templateUrl: './achievement-card.component.html',
   styleUrl: './achievement-card.component.scss'
 })
 export class AchievementCardComponent {
   @Input({required: true}) achievementData!: Achievement;
+  isModalOpened = false;
+  txStatus: boolean | undefined;
 
-  constructor(private database: DatabaseService, private telegram: TelegramService) { }
+  constructor(private database: DatabaseService, private telegram: TelegramService, private tonconnect: TonConnectService) { }
 
   showAchievementDescription() {
     this.telegram.showPopup(this.achievementData.name, this.achievementData.description,
@@ -26,6 +32,20 @@ export class AchievementCardComponent {
   }
 
   async claimSBT() {
-    this.database.claimSBT(this.telegram.getUserTGId()!, this.achievementData.id);
+    try {
+      this.isModalOpened = true;
+      const tx_hash = "_eb9e5a45fa21aca00407320f6645090cef37e41ecd24f06d0ce450776c3e8f59";// await this.database.claimSBT(this.telegram.getUserTGId()!, this.achievementData.id);
+      console.log(tx_hash);
+      this.txStatus = await this.tonconnect.getTxStatus(tx_hash);
+      console.log(this.txStatus);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  async closeModal() {
+    this.isModalOpened = false;
+    this.txStatus = undefined;
   }
 }
