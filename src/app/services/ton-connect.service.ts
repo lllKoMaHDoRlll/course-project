fa677a7101ac68bdcceaef20b620d1b15a03fee8
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-11-09 10:26:23 +0300

    implement getTxStatus method via tonApiClient

--- src/app/services/ton-connect.service.ts
+++ src/app/services/ton-connect.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { TonConnect, TonConnectUI, Wallet } from '@tonconnect/ui';
 import { DatabaseService } from './database/database.service';
 import { TelegramService } from './telegram.service';
+import { TonApiClient } from "@ton-api/client";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ export default class TonConnectService {
   connector;
   tonConnectUI;
   wallet: string | undefined;
+  tonApiClient: TonApiClient;
   constructor (private database: DatabaseService, private telegram: TelegramService) {
     this.connector = new TonConnect({manifestUrl: "https://tonolingo.ru/tonconnect-manifest.json"});
     this.connector.restoreConnection();
@@ -19,7 +21,11 @@ export default class TonConnectService {
         console.log(wallet.account.address);
         this.setWallet(wallet.account.address);
       }
-    })
+    });
+
+    this.tonApiClient = new TonApiClient({
+      baseUrl: "https://testnet.tonapi.io"
+    });
   }
 
   getWallet(): string | undefined {
@@ -30,6 +36,20 @@ export default class TonConnectService {
     if (!wallet || this.wallet == wallet) return;
     this.wallet = wallet;
     await this.database.createOrUpdateUser(this.telegram.getUserTGId()!, this.wallet);
+  }
+
+  async getTxStatus(tx_hash: string): Promise<boolean> {
+    console.log(1);
+    const response = await this.tonApiClient.traces.getTrace(tx_hash);
+    console.log(response);
+    if (!response) return false;
+    let status = response.transaction.success;
+    let tx = response;
+    while (status && tx.children && tx.children.length > 0) {
+      tx = tx.children[0];
+      status = tx.transaction.success;
+    }
+    return status;
   }
 
 }
